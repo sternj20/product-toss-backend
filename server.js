d88f4192c3669379368500187ee8885ab15c4e05
sternj20 <jessesternmusic@gmail.com>
2018-02-10 21:59:35 -0800

    changed server file

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 //Launch server on port 3000
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`app listening on port ${port}`))
