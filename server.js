afa63773fa9294afde9426915653e73660582708
sternj20 <jessesternmusic@gmail.com>
2018-02-11 09:58:41 -0800

    test data sending

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+// const routes = require('./routes');
 //Initialize server
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello world')
+const data = {items: [
+                {
+                    id: 1,
+                    label: 'List item 1'
+                },
+                {
+                    id: 2,
+                    label: 'List item 2'
+                },
+                {
+                    id: 3,
+                    label: 'List item 3'
+                },
+                {
+                    id: 4,
+                    label: 'List item 4'
+                }
+            ]}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes)
+app.get('/items', (req, res) => {
+	res.send(data)
 })
+
+
+mongoose.connect(process.env.MONGODB_URI,
+  { useMongoClient:true,
+    promiseLibrary: global.Promise }
+);
 
 const port = process.env.PORT || 3000;
 //Launch server on port 3000
