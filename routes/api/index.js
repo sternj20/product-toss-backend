c3beb6929ea29cb94f8ef55909e876b0d8351451
sternj20 <jessesternmusic@gmail.com>
2018-02-17 10:20:56 -0800

    changing vote boolean

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,7 +9,7 @@ router.get('/imgs', (req, res) => {
 })
 
 //Update # of votes in
-router.put('/imgs/:id/:value/:operation', (req, res) => {
+router.put('/imgs/:id/:value/', (req, res) => {
 	let query = {'_id': req.params.id}
 	let newData = {posVotes: parseInt(req.params.value) + 1, voted: true}
 	Image.findOneAndUpdate(query, newData, {upsert:true}, function(err, doc){
