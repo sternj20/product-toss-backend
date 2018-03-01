ec8de797a03075aea83369eafea03716d2cb7cbd
sternj20 <jessesternmusic@gmail.com>
2018-02-28 23:02:06 -0800

    sending res

--- routes/api/index.js
+++ routes/api/index.js
@@ -46,6 +46,7 @@ router.post('/imgs', upload.single('photo'), (req, res, next) => {
 router.post('/user/new', (req, res) => {
   let user = new User()
   user.save()
+  res.json(req.body)
 })
 
 //Update # of votes in
