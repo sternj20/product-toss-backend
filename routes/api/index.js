38735938023e4e36fe5ac22cfe467779c1b08024
sternj20 <jessesternmusic@gmail.com>
2018-02-28 22:55:50 -0800

    added route to add new user to db

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const Image = require('../../models/Image.js');
 const User = require('../../models/User.js');
-
 const aws = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
