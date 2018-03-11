8bfab98f841966ff0f71425d4e1af2de99dcbf5c
sternj20 <jessesternmusic@gmail.com>
2018-03-10 21:15:35 -0800

    console logging contest

--- routes/api/index.js
+++ routes/api/index.js
@@ -114,7 +114,7 @@ router.post("/contest/new/", (req, res) => {
     if(err) return err
     let contestId = contest._id
     let j = schedule.scheduleJob('11 * * * *', function(){
-      console.log(contestId)
+      console.log(contest._id)
     Contest.update({_id:contestId},{"$set":{"active":false}})
     console.log('The answer to life, the universe, and everything!');
     });
