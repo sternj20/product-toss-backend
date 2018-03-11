a0b24dcd2b7cd7a17739a13e80fba3a585395e6b
sternj20 <jessesternmusic@gmail.com>
2018-03-11 12:39:02 -0700

    fixing syntax

--- helpers/checkActive.js
+++ helpers/checkActive.js
@@ -1,5 +1,5 @@
 const Contest = require('../models/Contest.js');
-
+const fetch = require('node-fetch');
 function checkActive() {
 fetch('http://product-toss-backend.herokuapp.com/api/contest/check-active', { method: 'PUT' })
   .then(res => res.json())
