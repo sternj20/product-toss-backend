84ec30b800f1db6422392dc5e61fa4b2a613cac5
sternj20 <jessesternmusic@gmail.com>
2018-03-11 11:45:11 -0700

    using toisostring on current date

--- helpers/checkActive.js
+++ helpers/checkActive.js
@@ -2,21 +2,17 @@ const Contest = require('../models/Contest.js');
 
 function checkActive() {
   //Get current date
-  let currentDate = new Date().toISOString();
+  let currentDate = new Date()
   let newData = {active: false}
   //Find all contests where the expiration date is greater than the current date and set them to not be active
-  Contest.update(,{safe: true, upsert: true},
-      function(err, model) {
-        console.log(err);
-      });
-      console.log('Contest is over. Value of active has been changed from true to false')
+
+  Contest.findOneAndUpdate({expires: {$lte: currentDate}}, {$set:newData}, {upsert:true}, function(err, doc){
+      if (err) return res.send(500, { error: err });
+      return doc
+  });
 }
 checkActive();
 
-Contest.findOneAndUpdate({expires: {$lte: currentDate}}, {$set:newData}, {upsert:true}, function(err, doc){
-    if (err) return res.send(500, { error: err });
-    return doc
-});
 //Use moment
 //Add a field to the contest model that says created at
 //Find contest where created at is >= 7 days ago
